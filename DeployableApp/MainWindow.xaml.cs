182f41ba6baa28a6f17b36a72175c1ac8b9fa7a1
Leon van der Meer <64834803+leonvandermeer@users.noreply.github.com>
2023-10-13 21:06:04 +0200

    Display current version

--- DeployableApp/MainWindow.xaml.cs
+++ DeployableApp/MainWindow.xaml.cs
@@ -4,6 +4,8 @@
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window {
+    public static string AssemblyInformationalVersion { get; } = ThisAssembly.AssemblyInformationalVersion;
+
     public MainWindow() {
         InitializeComponent();
     }
