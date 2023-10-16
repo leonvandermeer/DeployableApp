67de064706bc714510c9d1b775a1ac80d5b7317e
Leon van der Meer <64834803+leonvandermeer@users.noreply.github.com>
2023-10-16 13:39:34 +0200

    Revise CreateCertificate

--- Certificates/CreateCertificate.ps1
+++ Certificates/CreateCertificate.ps1
@@ -4,29 +4,31 @@
 
 pushd $PSScriptRoot
 try {
-    $thumbprints = @()
-    try {
-        $password = Read-Host -Prompt 'Enter the password used to protect the certificate' -AsSecureString
-        $caCertificate = New-SelfSignedCertificate `
-            -DnsName 'CN=Leon''s Root Certificate Authority' `
-            -KeyUsage CRLSign, CertSign, DigitalSignature `
-            -TextExtension @("2.5.29.19={text}cA=true")
-        $thumbprints += $caCertificate.Thumbprint
-
-        $cert = New-SelfSignedCertificate `
-            -Signer $caCertificate `
-            -KeyUsage DigitalSignature,KeyEncipherment `
-            -DnsName localhost `
-            -Type SSLServerAuthentication `
-            -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3")
-        $thumbprints += $cert.Thumbprint
-
-        Export-PfxCertificate $cert DeployableApp.pfx -Password $password
-    } finally {
-        dir -Recurse -Force Cert:\ -Include $cert.Thumbprint, $caCertificate.Thumbprint | % PSPath | % {
-            del $_ -DeleteKey
-        }
+    if (![Security.Principal.WindowsPrincipal]::new([Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        throw 'This script must be run as Administrator.'
     }
+    if (!$env:CERTIFICATEPASSWORD) {
+        throw 'Store the Certificate''s Password in $env:CERTIFICATEPASSWORD'
+    }
+    $password = ConvertTo-SecureString $env:CERTIFICATEPASSWORD -AsPlainText -Force
+
+    $caCertificate = New-SelfSignedCertificate `
+        -DnsName 'CN=Leon''s Root Certificate Authority' `
+        -KeyUsage CRLSign, CertSign, DigitalSignature `
+        -Type CodeSigningCert `
+        -TextExtension @("2.5.29.19={text}cA=true")
+    '{0} {1:o}' -f $caCertificate.Thumbprint, (Get-Date) | Out-File -Encoding utf8 -Append Thumbprints.txt
+
+    $cert = New-SelfSignedCertificate `
+        -Signer $caCertificate `
+        -KeyUsage DigitalSignature `
+        -Subject 'Leon''s Code Signing Certificate' `
+        -Type CodeSigningCert
+    '{0} {1:o}' -f $cert.Thumbprint, (Get-Date) | Out-File -Encoding utf8 -Append Thumbprints.txt
+
+    $caCertificate = move $caCertificate.PSPath Cert:\LocalMachine\Root -PassThru
+
+    Export-PfxCertificate $cert DeployableApp.pfx -Password $password -ChainOption BuildChain
 } finally {
     popd
 }
