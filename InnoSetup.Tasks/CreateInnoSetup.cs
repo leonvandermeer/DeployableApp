b10284a41ebf7137bbe811ca904c6aa48c4bc9ca
Leon van der Meer <64834803+leonvandermeer@users.noreply.github.com>
2023-12-31 00:24:32 +0100

    Handle superfluous files during install

--- InnoSetup.Tasks/CreateInnoSetup.cs
+++ InnoSetup.Tasks/CreateInnoSetup.cs
@@ -25,6 +25,7 @@ public class CreateInnoSetup : Task {
         using TextWriter script = File.CreateText(scriptPath);
         SetupSection(script);
         FilesSection(script);
+        CodeSection(script);
     }
 
     private static void SetupSection(TextWriter script) {
@@ -42,7 +43,56 @@ public class CreateInnoSetup : Task {
             string source = item.GetMetadata("OutputPath");
             string relDir = Path.GetDirectoryName(item.GetMetadata("RelativePath"));
             string destDir = Path.Combine("{app}", relDir);
-            script.WriteLine("Source: \"{0}\"; DestDir: \"{1}\"; Flags: ignoreversion", source, destDir);
+            string destFile = Path.Combine(destDir, Path.GetFileName(source));
+            script.WriteLine("Source: \"{0}\"; DestDir: \"{1}\"; Flags: ignoreversion; AfterInstall: OnInstalled('{2}')", source, destDir, destFile);
         }
+    }
+
+    private void CodeSection(TextWriter script) {
+        script.WriteLine(@"
+[UninstallDelete]
+Type: files; Name: ""{app}\FileListAbsolute.txt""
+
+[Code]
+var
+  FileWrites: TStringList;
+  Superfluous: TStringList;
+
+procedure OnInstalled(FileName: String);
+var
+  Pos: Integer;
+begin
+  Log('OnInstalled: ' + FileName);
+  FileWrites.Add(ExpandConstant(FileName));
+  Pos := Superfluous.IndexOf(ExpandConstant(FileName));
+  if Pos >= 0 then
+    begin
+      Superfluous.Delete(Pos);
+    end;
+end;
+
+procedure CurStepChanged(CurStep: TSetupStep);
+var i: Integer;
+begin
+  Log('CurStepChanged(' + IntToStr(Ord(CurStep)) + ') called');
+  if CurStep = ssInstall then
+    begin
+      FileWrites := TStringList.Create;
+      Superfluous := TStringList.Create;
+      if FileExists(ExpandConstant('{app}\FileListAbsolute.txt')) then
+        begin
+          Superfluous.LoadFromFile(ExpandConstant('{app}\FileListAbsolute.txt'));
+        end;
+    end
+  else if CurStep = ssPostInstall then
+    begin
+      for i := 0 to Superfluous.Count - 1 do
+        begin
+          DeleteFile(Superfluous[i]);
+          RemoveDir(ExtractFileDir(Superfluous[i]));
+        end;
+      FileWrites.SaveToFile(ExpandConstant('{app}\FileListAbsolute.txt'));
+    end;
+end;");
     }
 }
